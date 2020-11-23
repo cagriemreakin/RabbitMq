89dff85524d9cb0cf4ecdfd75f4653800a0374b1
emre <cagriemreakin@hotmail.com>
2020-11-23 20:06:51 +0300

    fanout exchange

---
+++ RabbitMq.Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMq.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://vsezbtqj:Nf6BHHzO-RNCaW-rji1ewCnOTwRnup_7@grouse.rmq.cloudamqp.com/vsezbtqj");
+            //factory.HostName = "localhost";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //channel.QueueDeclare("task_queue", durable:true,exclusive: false,autoDelete: false, null);
+                    channel.ExchangeDeclare("logs", durable: true, type: ExchangeType.Fanout);
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queue:queueName, exchange:"logs", routingKey:"");
+
+                    channel.BasicQos(prefetchSize:0,prefetchCount:1,global:false);
+
+                    Console.WriteLine("Waiting logs");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume(queue:queueName,autoAck:false,consumer);
+
+                    consumer.Received += (model, ea) => {
+
+                        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        Console.WriteLine("Log alındı {0}", message);
+
+                        int time = int.Parse(GetMessage(args));
+                        Thread.Sleep(time);
+                        Console.WriteLine("Log bitti.");
+                        channel.BasicAck(ea.DeliveryTag,multiple:false);
+                    };
+                    Console.WriteLine("Consumer to exit!");
+                    Console.ReadLine();
+                }
+            }
+        }
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
+        }
+    }
+}
---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMq.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://vsezbtqj:Nf6BHHzO-RNCaW-rji1ewCnOTwRnup_7@grouse.rmq.cloudamqp.com/vsezbtqj");
+            //factory.HostName = "localhost";
+
+            // Eğer objemiz Idisposable ise işi bittikten sonra memoryden silinir.
+            // Bunun için using kullanılır
+            //IConnection : INetworkConnection, IDisposable
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //durable true olursa rabbit mq onu fiziksel bir diske yazar ve restart olduğunda veriler kaybolmaz. False olduğunda memory de durur
+                    //exclusive kuyruğa bir tane mi kanal bağlansın başka kanallarda bağlanabilrsin mi? False olursa diğerleride bağlanabilir.
+                    //autoDelete kuyruktaki işlemler bittiğinde kuyruk silinsin mi?
+
+                    //channel.QueueDeclare("task_queue",durable:true,false,false,null);
+                    channel.ExchangeDeclare("logs",durable:true,type:ExchangeType.Fanout);
+                    string message = GetMessage(args);
+
+                    for (int i =1; i < 11; i++)
+                    {
+                        byte[] body = Encoding.UTF8.GetBytes($"{message}-{i}");
+                        
+                        //mesajların silinmememsini sağlaıyor
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+
+                        //default exchange kullandığımız için ilkm parametre boş oldu
+                        channel.BasicPublish(exchange: "logs", routingKey: "task_queue", properties, body);
+                        Console.WriteLine($"{message}-{i}-gonderildi.");
+                    }
+                   
+
+                }
+                Console.WriteLine("Click to exit!");
+            }
+
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
+        }
+    }
+}
